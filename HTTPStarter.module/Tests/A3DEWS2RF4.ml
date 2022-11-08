0ca01321e261cae11aebab8e160d3dca384f7481
Haiderali Lakhani <Haiderali@BCTCL-DL342026.bctchn.local>
2022-11-08 22:20:09 +0530

    Latest maven test addition

--- HTTPStarter.module/Tests/A3DEWS2RF4.ml
+++ HTTPStarter.module/Tests/A3DEWS2RF4.ml
@@ -1,2 +1,6 @@
 <?xml version="1.0" encoding="ASCII"?>
-<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore"/>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="platform:/resource/HTTPStarter.module/Tests/SubProcess.bwt#/"/>
+  <EmulationDataFile>ACED000574001748545450537461727465722E6D6F64756C65312E627774</EmulationDataFile>
+  <EmulationDataFile>ACED000574000E53756250726F636573732E627774</EmulationDataFile>
+</emulation:BWTFile>
